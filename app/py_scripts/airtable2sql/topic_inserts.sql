bb8a472d6509cd2c3b6f96a622225c896b27fae5
francomorero <cwLancfZbgcJGduA2ZCG>
2020-03-30 10:31:05 -0300

    #8 remove topic_inserts.sql, we don't need anymore

--- app/py_scripts/airtable2sql/topic_inserts.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `topic` (`id`, `topic`) VALUES (1, 'Medical');
-INSERT INTO `topic` (`id`, `topic`) VALUES (2, 'Software');
-INSERT INTO `topic` (`id`, `topic`) VALUES (3, 'Digital');
-INSERT INTO `topic` (`id`, `topic`) VALUES (4, 'Solution');
-INSERT INTO `topic` (`id`, `topic`) VALUES (5, 'Collaboration');
-INSERT INTO `topic` (`id`, `topic`) VALUES (6, 'Data/Information');
-INSERT INTO `topic` (`id`, `topic`) VALUES (7, 'Support');
